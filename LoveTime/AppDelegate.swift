e1d0bccac428c6fd446cdfb8d14ddd3d62ccc303
Ocean li <li437277219@gmail.com>
2023-10-16 21:54:06 +0800

    “0.0.1”

--- LoveTime/AppDelegate.swift
+++ LoveTime/AppDelegate.swift
@@ -10,10 +10,19 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    var window: UIWindow?
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        self.window = UIWindow.init(frame: UIScreen.main.bounds)
+        self.window?.backgroundColor = UIColor.white
+        
+        let vc = ViewController()
+        let nav = UINavigationController(rootViewController: vc)
+        self.window?.rootViewController = nav
+        self.window?.makeKeyAndVisible()
+        
         return true
     }
 
---
+++ LoveTime/db/LTDataBaseTool.swift
@@ -0,0 +1,61 @@
+//
+//  LTDataBaseTool.swift
+//  LoveTime
+//
+//  Created by Ocean 李 on 2023/10/16.
+//
+
+import UIKit
+import WCDBSwift
+
+let USER_DATABASE_FLODER_NAME = "Database"
+
+class LTDataBaseTool : NSObject {
+    
+    static var `default` = LTDataBaseTool()
+    
+    private static let QUERY_LIMIT = 900
+    
+    var base: Database?
+    
+    func getDB() -> Database? {
+        return base
+    }
+    
+    func initDB(userID: String) {
+        
+        guard let dbBase = self.getDatabase(userId: userID) else {
+            return
+        }
+        self.base = dbBase
+//        HYUkCollectionDao.default.createTable(database: dbBase)
+//        HYUkSearchKeyDao.default.createTable(database: dbBase)
+//        HYUkHistoryRecordDao.default.createTable(database: dbBase)
+//        HYUkDownListDao.default.createTable(database: dbBase)
+//        HYUKNoticeListDao.default.createTable(database: dbBase)
+    }
+    
+    // MARK: -
+    private func getDatabasePath() -> String? {
+        
+//        let path = HYLocalPathManager.funVideoRootPath("DB")
+//        let createDbPath = HYLocalPathManager.createPath(path)
+//
+//        if createDbPath {
+//            print("Database Folder Path: \(path)")
+//            return path
+//        }
+        return nil
+    }
+    
+    /// Get a database with userID,  one user one database
+    func getDatabase(userId: String) -> Database? {
+        if let dbPath = self.getDatabasePath() {
+            let userDBPath = dbPath + "/" + userId + ".db"
+            let database = Database(withPath: userDBPath)
+            return database
+        }
+        return nil
+    }
+    
+}
